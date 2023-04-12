376bedd354c0d763e7665441ce5804d804438c83
codenoob1997 <h1013121231@live.com>
2023-04-12 09:48:53 +0800

    onlyVideos

--- pages/videos.js
+++ pages/videos.js
@@ -6,7 +6,7 @@ import BLOG from '@/blog.config'
 import VideosAvatar from '@/components/Hero/VideosAvatar'
 
 export async function getStaticProps() {
-  const posts = await getAllPosts({ onlyNewsletter: true })
+  const posts = await getAllPosts({ onlyVideos: true })
 
   const heros = await getAllPosts({ onlyHidden: true })
   const hero = heros.find((t) => t.slug === 'videos')
